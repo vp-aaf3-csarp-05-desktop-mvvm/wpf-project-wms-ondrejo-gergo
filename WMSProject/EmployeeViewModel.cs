b7bb1c135ca3b28ad36fc68f11a318687b006868
Gstr1k3r <ondrejogergo@gmail.com>
2026-02-03 10:40:01 +0100

    Több property megjelenítése.

--- WMSProject/EmployeeViewModel.cs
+++ WMSProject/EmployeeViewModel.cs
@@ -3,5 +3,9 @@
     public class EmployeeViewModel
     {
         public string Name { get; } = "Nagy Anna";
+
+        public string Adoszam { get; } = "1234567-89-01";
+
+        public int SzabadnapokSzama { get; } = 15;
     }
 }
